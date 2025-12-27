0557c46baf6ac470729daac4354a83698f1a502b
renewalv <denbar259@gmail.com>
2025-12-27 15:26:22 +0200

    simple camera webserver

---
+++ CameraWebServer/CameraWebServer.ino
@@ -0,0 +1,177 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <Preferences.h>
+
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     5
+#define Y9_GPIO_NUM       4
+#define Y8_GPIO_NUM       6
+#define Y7_GPIO_NUM       7
+#define Y6_GPIO_NUM       14
+#define Y5_GPIO_NUM       17
+#define Y4_GPIO_NUM       21
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM       16
+#define VSYNC_GPIO_NUM    1
+#define HREF_GPIO_NUM     2
+#define PCLK_GPIO_NUM     15
+#define SIOD_GPIO_NUM  8
+#define SIOC_GPIO_NUM  9
+
+#define LED_GPIO_NUM  47
+
+Preferences preferences;
+const char* ssid;
+const char* password;
+
+void startCameraServer();
+void setupLedFlash(int pin);
+
+void connectToWiFi(const char* ssid, const char* password) {
+  WiFi.begin(ssid, password);
+  Serial.printf("Connecting to WiFi: %s\n", ssid);
+  int retries = 0;
+  while (WiFi.status() != WL_CONNECTED && retries < 20) {
+    delay(500);
+    Serial.print(".");
+    retries++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nFailed to connect to WiFi.");
+  }
+}
+
+void initWiFi() {
+  preferences.begin("wifi", false);
+  String savedSSID = "TP-Link_E1BF";  //preferences.getString("ssid", "");
+  String savedPASS = "47082595"; //preferences.getString("password", "");
+
+  if (savedSSID.length() > 0 && savedPASS.length() > 0) {
+    Serial.println("Found saved WiFi credentials.");
+     connectToWiFi("TP-Link_E1BF","47082595");
+
+    if (WiFi.status() == WL_CONNECTED) {
+      return;
+    } else {
+      Serial.println("Stored credentials failed. Please enter new credentials.");
+    }
+  } else {
+    Serial.println("No WiFi credentials found. Please enter:");
+  }
+
+  while (Serial.available()) Serial.read();
+
+  Serial.println("Enter SSID: ");
+  while (Serial.available() == 0) delay(10);
+  String inputSSID = Serial.readStringUntil('\n');
+  inputSSID.trim();
+
+  Serial.println("Enter Password: ");
+  while (Serial.available() == 0) delay(10);
+  String inputPASS = Serial.readStringUntil('\n');
+  inputPASS.trim();
+
+  connectToWiFi("TP-Link_E1BF","47082595");
+  
+
+  if (WiFi.status() == WL_CONNECTED) {
+    preferences.putString("ssid", inputSSID);
+    preferences.putString("password", inputPASS);
+    Serial.println("WiFi credentials saved.");
+  } else {
+    Serial.println("Failed to connect. Credentials not saved.");
+  }
+
+  preferences.end();
+}
+
+void initCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  delay(1000);
+  Serial.println();
+  
+  initCamera();
+  //setupLedFlash(LED_GPIO_NUM);
+  initWiFi();
+  startCameraServer();
+}
+
+void loop() {
+  // Do nothing. Everything is done in another task by the web server
+  delay(10000);
+}
